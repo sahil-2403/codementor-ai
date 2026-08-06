83312d3e711c6ebbdc26561772cabdc00ffaac1b
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-06 20:00:55 +0530

    feat: mount app toaster

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppErrorBoundary from './components/common/AppErrorBoundary.jsx';
+import AppToaster from './components/feedback/AppToaster.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
 import App from './App.jsx';
 import './index.css';
@@ -24,6 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <AuthProvider>
             <App />
           </AuthProvider>
+          <AppToaster />
         </BrowserRouter>
       </QueryClientProvider>
     </AppErrorBoundary>
