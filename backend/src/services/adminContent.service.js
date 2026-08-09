7465ca7ed87b9b66ac0d37653c6efc4fbb232397
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-09 22:04:15 +0530

    feat: export admin content overview

--- backend/src/services/adminContent.service.js
+++ backend/src/services/adminContent.service.js
@@ -3,3 +3,4 @@ export * from './adminContent/lesson.service.js';
 export * from './adminContent/question.service.js';
 export * from './adminContent/interviewQuestion.service.js';
 export * from './adminContent/template.service.js';
+export * from './adminContent/overview.service.js';
