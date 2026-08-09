cdfc45755a257ba0ae0f99e88eee62bf4e530631
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-09 19:35:06 +0530

    feat: add quiz questions management page

---
+++ frontend/src/pages/admin/QuizQuestionsPage.jsx
@@ -0,0 +1,5 @@
+import QuestionBankManagement from '../../components/admin/QuestionBankManagement.jsx';
+
+export default function QuizQuestionsPage() {
+  return <QuestionBankManagement bank="quiz" />;
+}
