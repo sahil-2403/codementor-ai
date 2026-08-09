2b6d00aa80f5c7cbcee5bea3fdf4dfb9b9f48713
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-10 00:45:45 +0530

    refactor: use enrollment assessment api

--- frontend/src/api/assessmentApi.js
+++ frontend/src/api/assessmentApi.js
@@ -1,6 +1,7 @@
 import api from './axiosInstance.js';
+
 export const assessmentApi = {
-  start: ({ level, learningGoalId }) => api.get('/assessments/start', { params: { level, learningGoalId } }).then((res) => res.data.data),
+  start: ({ level, enrollmentId }) => api.get('/assessments/start', { params: { level, enrollmentId } }).then((res) => res.data.data),
   submit: (payload) => api.post('/assessments/submit', payload).then((res) => res.data.data),
   report: (assessmentId) => api.get(`/assessments/${assessmentId}/report`).then((res) => res.data.data)
 };
