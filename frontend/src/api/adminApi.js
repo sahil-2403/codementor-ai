f1829c9994845cde9afebc69e47935358dded1ce
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-10 00:48:06 +0530

    feat: add admin catalog api client

--- frontend/src/api/adminApi.js
+++ frontend/src/api/adminApi.js
@@ -5,6 +5,27 @@ const withParams = (params = {}) => ({ params: Object.fromEntries(Object.entries
 export const adminApi = {
   contentOverview: () => api.get('/admin/content-overview').then((res) => res.data.data),
 
+  technologies: (params) => api.get('/admin/technologies', withParams(params)).then((res) => res.data.data),
+  technology: (id) => api.get(`/admin/technologies/${id}`).then((res) => res.data.data),
+  createTechnology: (payload) => api.post('/admin/technologies', payload).then((res) => res.data.data),
+  updateTechnology: ({ id, payload }) => api.patch(`/admin/technologies/${id}`, payload).then((res) => res.data.data),
+  updateTechnologyStatus: ({ id, status, confirmPublish = false }) => api.patch(`/admin/technologies/${id}/status`, { status, confirmPublish }).then((res) => res.data.data),
+  deleteTechnology: (id) => api.delete(`/admin/technologies/${id}`).then((res) => res.data.data),
+
+  courses: (params) => api.get('/admin/courses', withParams(params)).then((res) => res.data.data),
+  course: (id) => api.get(`/admin/courses/${id}`).then((res) => res.data.data),
+  createCourse: (payload) => api.post('/admin/courses', payload).then((res) => res.data.data),
+  updateCourse: ({ id, payload }) => api.patch(`/admin/courses/${id}`, payload).then((res) => res.data.data),
+  updateCourseStatus: ({ id, status, confirmPublish = false }) => api.patch(`/admin/courses/${id}/status`, { status, confirmPublish }).then((res) => res.data.data),
+  deleteCourse: (id) => api.delete(`/admin/courses/${id}`).then((res) => res.data.data),
+
+  learningPaths: (params) => api.get('/admin/learning-paths', withParams(params)).then((res) => res.data.data),
+  learningPath: (id) => api.get(`/admin/learning-paths/${id}`).then((res) => res.data.data),
+  createLearningPath: (payload) => api.post('/admin/learning-paths', payload).then((res) => res.data.data),
+  updateLearningPath: ({ id, payload }) => api.patch(`/admin/learning-paths/${id}`, payload).then((res) => res.data.data),
+  updateLearningPathStatus: ({ id, status, confirmPublish = false }) => api.patch(`/admin/learning-paths/${id}/status`, { status, confirmPublish }).then((res) => res.data.data),
+  deleteLearningPath: (id) => api.delete(`/admin/learning-paths/${id}`).then((res) => res.data.data),
+
   topics: (params) => api.get('/admin/topics', withParams(params)).then((res) => res.data.data),
   topic: (id) => api.get(`/admin/topics/${id}`).then((res) => res.data.data),
   topicImpact: (id) => api.get(`/admin/topics/${id}/impact`).then((res) => res.data.data),
