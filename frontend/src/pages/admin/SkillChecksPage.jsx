508dcb524c72a0dc674fb57c096834735bc9e6c6
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-09 19:35:18 +0530

    feat: add skill checks management page

---
+++ frontend/src/pages/admin/SkillChecksPage.jsx
@@ -0,0 +1,5 @@
+import QuestionBankManagement from '../../components/admin/QuestionBankManagement.jsx';
+
+export default function SkillChecksPage() {
+  return <QuestionBankManagement bank="skill_check" />;
+}
