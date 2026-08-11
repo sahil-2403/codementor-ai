74020f4b5d536ef5f7d98a298b5f6f8b9abcc5c6
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-12 00:46:27 +0530

    refactor: remove duplicate progress data hook

--- frontend/src/hooks/useProgressData.js
+++
@@ -1,5 +0,0 @@
-import { progressApi } from '../api/progressApi.js';
-import { useAsyncAction } from './useAsyncAction.js';
-export { useDashboard } from './useDashboardData.js';
-
-export const useUpdateRevision = () => useAsyncAction(progressApi.updateRevision);
