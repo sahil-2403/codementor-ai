eea6f92116444df323bb1e1fc20c098fc890e308
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-12 00:46:52 +0530

    refactor: remove duplicate report data hook

--- frontend/src/hooks/useReportData.js
+++
@@ -1,6 +0,0 @@
-import { reportApi } from '../api/reportApi.js';
-import { useAsyncAction } from './useAsyncAction.js';
-import { useAsyncData } from './useAsyncData.js';
-
-export const useReports = () => useAsyncData(reportApi.list);
-export const useGenerateReport = () => useAsyncAction(reportApi.generate);
