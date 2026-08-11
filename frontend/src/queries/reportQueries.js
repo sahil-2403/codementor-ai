91be29bae014817fabd88bf5f39f86967391f14a
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-12 00:29:52 +0530

    refactor: load reports without query library

--- frontend/src/queries/reportQueries.js
+++ frontend/src/queries/reportQueries.js
@@ -1,8 +1,6 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { reportApi } from '../api/reportApi.js';
-import { queryKeys } from '../constants/queryKeys.js';
-export const useReports = () => useQuery({ queryKey: queryKeys.reports, queryFn: reportApi.list });
-export const useGenerateReport = () => {
-  const queryClient = useQueryClient();
-  return useMutation({ mutationFn: reportApi.generate, onSuccess: () => queryClient.invalidateQueries({ queryKey: queryKeys.reports }) });
-};
+import { useAsyncAction } from '../hooks/useAsyncAction.js';
+import { useAsyncData } from '../hooks/useAsyncData.js';
+
+export const useReports = () => useAsyncData(reportApi.list);
+export const useGenerateReport = () => useAsyncAction(reportApi.generate);
