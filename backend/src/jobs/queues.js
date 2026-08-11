a1bc26b95c768246eff13d3ac959948ff58cb886
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-12 00:17:19 +0530

    refactor: remove queue infrastructure

--- backend/src/jobs/queues.js
+++
@@ -1,9 +0,0 @@
-import { Queue } from 'bullmq';
-import { getRedisConnection } from '../config/redis.js';
-
-const connection = getRedisConnection();
-
-export const createQueue = (name) => {
-  if (!connection) return null;
-  return new Queue(name, { connection });
-};
