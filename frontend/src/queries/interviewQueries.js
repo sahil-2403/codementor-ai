4040b464dd7e0a54d7f7cacfe8c0b476014ad5a0
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-12 00:29:23 +0530

    refactor: load interview data without query library

--- frontend/src/queries/interviewQueries.js
+++ frontend/src/queries/interviewQueries.js
@@ -1,32 +1,12 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { interviewApi } from '../api/interviewApi.js';
-import { queryKeys } from '../constants/queryKeys.js';
-import { STALE_TIMES } from '../constants/queryConfig.js';
-import { invalidateMany } from './queryUtils.js';
+import { useAsyncAction } from '../hooks/useAsyncAction.js';
+import { useAsyncData } from '../hooks/useAsyncData.js';
 
-export const useInterviewQuestions = (params = {}) => useQuery({
-  queryKey: queryKeys.interviewQuestions(params),
-  queryFn: () => interviewApi.questions(params),
-  staleTime: STALE_TIMES.MEDIUM
-});
-
-export const useInterviewAttempts = () => useQuery({
-  queryKey: queryKeys.interviewAttempts,
-  queryFn: interviewApi.attempts
-});
-
-export const useSubmitInterviewAnswer = () => {
-  const queryClient = useQueryClient();
-  return useMutation({
-    mutationFn: interviewApi.submit,
-    onSuccess: () => invalidateMany(queryClient, [queryKeys.interviewAttempts, queryKeys.dashboard])
-  });
+export const useInterviewQuestions = (params = {}) => {
+  const key = JSON.stringify(params);
+  return useAsyncData(() => interviewApi.questions(params), [key]);
 };
 
-export const useRetryInterviewReview = () => {
-  const queryClient = useQueryClient();
-  return useMutation({
-    mutationFn: interviewApi.retryReview,
-    onSuccess: () => invalidateMany(queryClient, [queryKeys.interviewAttempts, queryKeys.dashboard])
-  });
-};
+export const useInterviewAttempts = () => useAsyncData(interviewApi.attempts);
+export const useSubmitInterviewAnswer = () => useAsyncAction(interviewApi.submit);
+export const useRetryInterviewReview = () => useAsyncAction(interviewApi.retryReview);
