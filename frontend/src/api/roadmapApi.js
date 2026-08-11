5e68528bda02c8700a4bdc5eebccf433d35d80b0
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-12 00:31:03 +0530

    refactor: remove roadmap job api

--- frontend/src/api/roadmapApi.js
+++ frontend/src/api/roadmapApi.js
@@ -1,10 +1,9 @@
 import api from './axiosInstance.js';
+
 export const roadmapApi = {
   current: () => api.get('/roadmaps/current').then((res) => res.data.data),
   versions: () => api.get('/roadmaps/versions').then((res) => res.data.data),
   generateOrGet: () => api.post('/roadmaps/generate-or-get').then((res) => res.data.data),
   fromAssessment: (payload) => api.post('/roadmaps/from-assessment', payload).then((res) => res.data.data),
-  personalizeLater: () => api.post('/roadmaps/personalize-later').then((res) => res.data.data),
-  jobStatus: (jobId) => api.get(`/roadmaps/jobs/${jobId}`).then((res) => res.data.data),
-  retryJob: (jobId) => api.post(`/roadmaps/jobs/${jobId}/retry`).then((res) => res.data.data)
+  personalizeLater: () => api.post('/roadmaps/personalize-later').then((res) => res.data.data)
 };
