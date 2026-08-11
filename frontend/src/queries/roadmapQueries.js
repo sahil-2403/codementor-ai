a7f9f8753a10e449b2c842ab25376ed1f1f67cf4
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-12 00:29:12 +0530

    refactor: load roadmap without query library

--- frontend/src/queries/roadmapQueries.js
+++ frontend/src/queries/roadmapQueries.js
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
 import { roadmapApi } from '../api/roadmapApi.js';
-import { queryKeys } from '../constants/queryKeys.js';
-export const useRoadmap = () => useQuery({ queryKey: queryKeys.roadmap, queryFn: roadmapApi.current, staleTime: 1000 * 60 });
+import { useAsyncData } from '../hooks/useAsyncData.js';
+
+export const useRoadmap = () => useAsyncData(roadmapApi.current);
