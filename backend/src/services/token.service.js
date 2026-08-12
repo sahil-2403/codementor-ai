f9e14f3a7aa3afe6860ecf28cfe3bee8e3077e28
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-13 00:23:40 +0530

    refactor: use simple token version sessions

--- backend/src/services/token.service.js
+++ backend/src/services/token.service.js
@@ -1,24 +1,27 @@
 import jwt from 'jsonwebtoken';
 import { env } from '../config/env.js';
 import { accessCookieOptions, clearCookieOptions, refreshCookieOptions } from '../config/cookies.js';
-import { sha256 } from '../utils/hash.js';
 
 export const createAccessToken = (user) =>
   jwt.sign(
-    { id: user._id, role: user.role, tokenVersion: user.refreshTokenVersion || 0 },
+    { id: user._id, role: user.role, tokenVersion: user.tokenVersion || 0 },
     env.jwtAccessSecret,
     { expiresIn: env.jwtAccessExpiresIn }
   );
 
 export const createRefreshToken = (user) =>
-  jwt.sign({ id: user._id, version: user.refreshTokenVersion || 0 }, env.jwtRefreshSecret, {
-    expiresIn: env.jwtRefreshExpiresIn
-  });
+  jwt.sign(
+    { id: user._id, tokenVersion: user.tokenVersion || 0 },
+    env.jwtRefreshSecret,
+    { expiresIn: env.jwtRefreshExpiresIn }
+  );
 
-export const hashToken = (token) => sha256(token);
+export const setAccessTokenCookie = (res, accessToken) => {
+  res.cookie('accessToken', accessToken, accessCookieOptions());
+};
 
 export const setAuthCookies = (res, accessToken, refreshToken) => {
-  res.cookie('accessToken', accessToken, accessCookieOptions());
+  setAccessTokenCookie(res, accessToken);
   res.cookie('refreshToken', refreshToken, refreshCookieOptions());
 };
 
