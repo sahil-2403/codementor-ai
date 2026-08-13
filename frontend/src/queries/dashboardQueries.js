bd0367062d66e083f7dbb41be1d5fe0b45f19ce7
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-13 20:50:13 +0530

    refactor: remove dashboard query wrapper

--- frontend/src/queries/dashboardQueries.js
+++
@@ -1,4 +0,0 @@
-import { progressApi } from '../api/progressApi.js';
-import { useAsyncData } from '../hooks/useAsyncData.js';
-
-export const useDashboard = () => useAsyncData(progressApi.dashboard);
