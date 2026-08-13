953fc44cf3272b7f43bace4a0d51fc0d095a11c7
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-13 20:51:15 +0530

    refactor: remove progress query wrapper

--- frontend/src/queries/progressQueries.js
+++
@@ -1,5 +0,0 @@
-import { progressApi } from '../api/progressApi.js';
-import { useAsyncAction } from '../hooks/useAsyncAction.js';
-export { useDashboard } from './dashboardQueries.js';
-
-export const useUpdateRevision = () => useAsyncAction(progressApi.updateRevision);
