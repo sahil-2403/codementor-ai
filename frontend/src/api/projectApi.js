904fc739947ab82fe4a95eac7bf20615a3125cbe
Sahil2426 <pawarsahil2430@gmail.com>
2026-08-15 14:09:31 +0530

    refactor: projects to practice

--- frontend/src/api/projectApi.js
+++
@@ -1,8 +0,0 @@
-import api from './axiosInstance.js';
-
-export const projectApi = {
-  tasks: (params = {}) => api.get('/projects/tasks', { params }).then((res) => res.data.data),
-  task: (taskId) => api.get(`/projects/tasks/${taskId}`).then((res) => res.data.data),
-  submit: (payload) => api.post('/projects/submissions', payload).then((res) => res.data.data),
-  review: (submissionId) => api.post(`/projects/submissions/${submissionId}/review`).then((res) => res.data.data)
-};
