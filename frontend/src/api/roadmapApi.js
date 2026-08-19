0b078e85ceb2c108bd9d31125a5ce8635fc9fc1b
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-19 11:43:24 +0530

    refactor: trim roadmap api wrapper

--- frontend/src/api/roadmapApi.js
+++ frontend/src/api/roadmapApi.js
@@ -2,8 +2,6 @@ import api from './axiosInstance.js';
 
 export const roadmapApi = {
   current: () => api.get('/roadmaps/current').then((res) => res.data.data),
-  versions: () => api.get('/roadmaps/versions').then((res) => res.data.data),
   generateOrGet: () => api.post('/roadmaps/generate-or-get').then((res) => res.data.data),
-  fromAssessment: (payload) => api.post('/roadmaps/from-assessment', payload).then((res) => res.data.data),
-  personalizeLater: () => api.post('/roadmaps/personalize-later').then((res) => res.data.data)
+  fromAssessment: (payload) => api.post('/roadmaps/from-assessment', payload).then((res) => res.data.data)
 };
