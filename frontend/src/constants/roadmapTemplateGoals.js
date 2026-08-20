2db2237440c899bda837244c31c1bf00f6a263e0
Sahil Pawar <pawarsahil2430@gmail.com>
2026-08-20 22:59:07 +0530

    chore: remove stale roadmap goal constants

--- frontend/src/constants/roadmapTemplateGoals.js
+++
@@ -1,7 +0,0 @@
-export const ROADMAP_TEMPLATE_GOALS = [
-  { key: 'junior-mern-stack', label: 'Junior MERN Stack Developer' }
-];
-
-export const getRoadmapTemplateGoalLabel = (goalKey) => (
-  ROADMAP_TEMPLATE_GOALS.find((goal) => goal.key === goalKey)?.label || goalKey
-);
